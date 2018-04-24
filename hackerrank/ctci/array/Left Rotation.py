710e35c10416662feb2ca00d7e34d4692d79e6b4
Gauravpande <garvpande11235@gmail.com>
2018-04-24 16:17:51 +0530

    Create  Left Rotation.py

---
+++ hackerrank/ctci/array/Left Rotation.py
@@ -0,0 +1,13 @@
+# [Problem link](https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem)
+
+def array_left_rotation(a, n, k):
+    while k > 0:
+        a.append(a.pop(0))
+        k = k - 1
+    return a
+    
+    
+n, k = map(int, input().strip().split(' '))
+a = list(map(int, input().strip().split(' ')))
+answer = array_left_rotation(a, n, k);
+print(*answer, sep=' ')
