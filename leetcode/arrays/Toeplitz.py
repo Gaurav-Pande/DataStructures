36bb7395b339e2999be8d23f82be6c1cd639af89
Gauravpande <garvpande11235@gmail.com>
2018-04-29 12:08:36 +0530

    Create Toeplitz.py

---
+++ leetcode/arrays/Toeplitz.py
@@ -0,0 +1,11 @@
+#(766. Toeplitz Matrix)[https://leetcode.com/problems/toeplitz-matrix/description/]
+
+class Solution:
+    def isToeplitzMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: bool
+        """
+        return all(r == 0 or c == 0 or matrix[r-1][c-1]== val
+               for r, id in enumerate(matrix)
+               for c, val in enumerate(id))
