13ac545c5a4c3d89284e7931f6ccdb3b91466995
Gauravpande <garvpande11235@gmail.com>
2018-04-30 10:49:08 +0530

    Create reverse1.py

---
+++ leetcode/strings/reverse1.py
@@ -0,0 +1,16 @@
+# [344. Reverse String](https://leetcode.com/problems/reverse-string/description/)
+
+class Solution:
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        i = len(s) - 1
+        j = 0
+        res = ""
+        while i >= 0:
+            res = res +  str(s[i])
+            i = i - 1
+            j = j + 1
+        return res
